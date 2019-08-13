b0420bf81a3c524d0a15e34eae64601f6819d825
vagrant <vagrant@homestead>
2019-08-13 16:38:49 +0000

    route for mailgun

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ Auth::routes();
 Route::get('/', 'PageController@home');
 
 Route::post('/contactMessage', 'MessageController@contact');
+
+Route::post('/storeEmail', 'MessageController@storeEmail');
